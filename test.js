6d8595ef7733e4735be2044e8b10419279e845e8
amitmjose <amitmjose@gmail.com>
2022-07-09 19:10:22 +0530

    javascript tutorial

---
+++ test.js
@@ -0,0 +1,227 @@
+// alert("hello, world");
+
+// var youlikemeat = true;
+// var myNum = 10;
+
+// if (myNum>10){
+
+//     document.write("here's the meat menu...");
+    
+
+// } 
+// else {
+//     document.write("false")
+// }
+
+
+
+// if-else
+
+// var myAge = 25;
+
+// // AND operator &&
+
+// if (myAge>=18 && myAge<=30){
+
+//     document.write("lets partaayyy.....")
+
+// } else if(myAge<18){
+    
+//     document.write("go home!!..you're grounded")
+
+// } else{
+
+//     document.write("boo boo boomer...")
+
+// }
+
+
+
+// AND operator &&
+// // OR operator ||
+
+// if (myAge < 18 || myAge >30 || myAge==25){
+
+//     document.write("go home!!.")
+
+// } else{
+
+//     document.write("lets partaayyy.....")
+
+// }
+
+
+// // while loop
+
+// var age = 5;
+
+// while(age < 10){
+
+//     console.log("age less than 10");
+//     age++;
+// }
+
+// document.write("you are no wover 10");
+
+
+
+
+//for loop
+
+// var links= document.getElementsByTagName("a");
+
+// for (i = 1; i <= links.length; i++) {
+    
+//     console.log("this is link no."  +i);
+
+// }
+
+// document.write("all links now looped")
+
+
+
+// // continue break
+// for (i = 0; i < 10; i++) {
+
+//     if(i === 3 || i === 5) {
+        
+//         continue;
+
+//     }   
+
+
+//     document.write(i);
+
+//     if(i === 7) {
+
+//         break;
+//     }
+// }
+// document.write("loop broken")
+
+
+
+//
+
+// var links = document.getElementsByTagName("a");
+
+// for (i = 0; i < links.length; i++){
+
+//     links[i].className = "link" + i;
+// }
+
+
+
+// //functions
+
+// function getAverage (a,b,c,d,e,f){
+
+//     var average = (a+b+c+d+e+f) / 2;
+//     console.log(average);
+//     return average;
+
+// }
+
+// var avg = getAverage(7,8,9,2,5,5); 
+// document.write("the avg. is" + avg);
+
+
+
+// //strings
+
+// var mystring = 'I\'m a "fun" string';
+// var space = '----';
+
+// document.write(mystring)
+// document.write(space)
+
+// document.write("length of string : " + mystring.length);
+// document.write(space);
+// document.write("transform to uppercase : " + mystring.toUpperCase());
+// document.write(space);
+// document.write("transform to lowerrcase : " + mystring.toLowerCase());
+// document.write(space);
+// document.write("index the letter r : " + mystring.indexOf('r'));
+
+
+// var s1 = "abc";
+// var s2 = 'bcd';
+// var s3 = "Abc";
+// var s4 = "ABC";
+// var s5 = "aBC";
+
+
+// document.write(s1>s2);
+// document.write(s1<s2);
+// document.write(s1>s5);
+// document.write(s5>s2);
+// document.write(s1>s4);
+// document.write(s1==s4);
+
+
+
+
+// //slice & split
+
+
+// var str = "hello , world";
+
+// document.write(str);
+
+// var str2 = str.slice(2 , 10);
+// document.write(str2);
+
+// var str3 = str.slice(2);
+// document.write ( str3);
+
+
+// var tags = "meat , ham , salami , pork , beef , chicken";
+
+// document.write(tags);
+
+// var tagsArray = tags.split(",");
+// document.write(tagsArray);
+
+
+
+// var tags = "meat , ham , salami , pork , beef , chicken";
+
+// console.log(tags);
+
+// var tagsArray = tags.split(",");
+// console.log(tagsArray);
+
+
+
+// // arrays
+
+// var myArray = [];
+// myArray =[10 , 50 , "sdkjdc" , true];
+
+
+// myArray[0] = 56;
+
+// document.write(myArray);
+// myArray[2]= "poda";
+// document.write(myArray);
+
+// var myarray2 = new Array();
+
+// var myArray3 = new Array(5);
+
+// document.write(myArray3.length);
+ 
+
+
+// // Objects
+
+// var myStr = new String()
+// myStr = "asdfjkl;";
+// document.write(myStr);
+
+
+// var myString= "hi there";
+// document.write (myString)
+
+
+var myArray 
